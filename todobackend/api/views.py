bb638b1df433298e159c6020ec2d3c56d324af94
BenjaminSemevor <benjaminsemevor@ymail.com>
2024-07-20 16:14:22 +0000

    Completing a Todo

--- todobackend/api/views.py
+++ todobackend/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions
-from .serializers import TodoSerializer
+from .serializers import TodoSerializer, TodoToggleCompleteSerializer
 from todo.models import Todo
 class TodoListCreate(generics.ListCreateAPIView):
     # ListAPIView requires two mandatory attributes, serializer_class and
@@ -23,3 +23,14 @@ class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
         user = self.request.user
         # user can only update, delete own posts
         return Todo.objects.filter(user=user)
+class TodoToggleComplete(generics.UpdateAPIView):
+    serializer_class = TodoToggleCompleteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
+
+    def perform_update(self,serializer):
+        serializer.instance.completed=not(serializer.instance.completed)
+        serializer.save()
